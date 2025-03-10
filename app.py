59b7bc422f300f416b79976f43df44e28e920787
younes_qasempour <qasempouryounes@gmail.com>
2025-03-10 15:20:07 +0330

    Working

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import random
+import datetime
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    random_number = random.randint(1, 10)
+    year = datetime.datetime.now().year
+    return render_template("index.html", num=random_number, year=year)
 
 
 if __name__ == '__main__':
